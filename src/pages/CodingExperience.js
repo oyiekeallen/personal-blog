37d915d09e1c1b4b6610f57d4fd4964adef9ada8
Allen Oyieke <oyiekeallen@gmail.com>
2021-05-09 14:30:07 +0300

    feat: Complete Experience Page - Add images for previous employement - Update description of tasks performed at each of the previous companies

--- src/pages/CodingExperience.js
+++ src/pages/CodingExperience.js
@@ -3,30 +3,31 @@ import React from "react"
 import { Chrono } from 'react-chrono'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHistory } from '@fortawesome/free-solid-svg-icons'
+import { FMLogoImage } from '../images/the_french_maiden_logo.png'
 
 export default function CoingExperience(){
 	const items = [
 		{
 			title: "May 2018",
 			cardTitle: "The French Maiden Limited",
-			cardSubtitle:"Full-stack Laravel & Mobile Application Developer",
-			cardDetailedText: "...",
+			cardSubtitle:"Full Stack Laravel & Mobile Application Developer",
+			cardDetailedText: "I was contracted to help the company launch its online presence in the home services space. I worked on developing a web application as well as a mobile application on the Android Platform.",
 			media: {
 				type: "IMAGE",
 				source: {
-					url: "http://someurl/image.jpg"
+					url: "/images/the_french_maiden_logo.png"
 				}
 			}
 		},
 		{
 			title: "September 2019",
-			cardTitle: "Odyssey Capital Limited",
-			cardSubtitle:"Software Engineer",
-			cardDetailedText: "...",
+			cardTitle: "Odyssey Capital Limited   ",
+			cardSubtitle:"Full Stack Software Developer",
+			cardDetailedText: "I worked as a full-stack developer, primarily developing software in Ruby and Angular.",
 			media: {
 				type: "IMAGE",
 				source: {
-					url: "http://someurl/image.jpg"
+					url: "/images/odyssey_capital_logo_blue.png"
 				}
 			}
 		}
